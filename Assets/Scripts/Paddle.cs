f8104751d29e67a017867dc90a9e240c92db17d4
Givaldo Fernandes <gf.givaldofernandes@gmail.com>
2024-12-14 01:36:46 -0300

    Paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,13 +3,13 @@ using UnityEngine.Serialization;
 
 public class Paddle : MonoBehaviour
 {
-    [SerializeField] string inputAxis;
-    [SerializeField] private float movimentSpeedScale = 5f;
+    [SerializeField] string _inputAxis;
+    [SerializeField] private float _moveSpeed = 5f;
     private float _yLimit = 3.7f;
     void FixedUpdate()
     {
-       if(Input.GetAxis(inputAxis) != 0){
-           float move = Input.GetAxisRaw(inputAxis) * movimentSpeedScale * Time.deltaTime;
+       if(Input.GetAxis(_inputAxis) != 0){
+           float move = Input.GetAxisRaw(_inputAxis) * _moveSpeed * Time.deltaTime;
            Vector3 newPosition = transform.position + new Vector3(0, move, 0);
            newPosition.y = Mathf.Clamp(newPosition.y, -_yLimit, _yLimit);
            transform.position = newPosition;
